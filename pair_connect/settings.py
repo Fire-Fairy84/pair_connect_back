7f304c8b490214e65eda5621aa7ce57d505c726e
Fire-Fairy84 <esther.sarasua@gmail.com>
2024-09-23 13:23:37 +0200

    config email confirmation

--- pair_connect/settings.py
+++ pair_connect/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -17,6 +18,8 @@ import cloudinary
 import cloudinary_storage
 import cloudinary.api
 from datetime import timedelta
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -189,3 +192,12 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
---
+++ users/signals.py
@@ -0,0 +1,15 @@
+# users/signals.py
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import CustomUser
+from .utils import send_confirmation_email
+import jwt
+from django.conf import settings
+
+@receiver(post_save, sender=CustomUser)
+def send_welcome_email(sender, instance, created, **kwargs):
+    if created:
+        # Generar un token de confirmación (este es un ejemplo, personaliza según tu lógica)
+        token = jwt.encode({'user_id': instance.id}, settings.SECRET_KEY, algorithm='HS256')
+        send_confirmation_email(instance, token)
