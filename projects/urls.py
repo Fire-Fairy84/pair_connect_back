c224958440f006166ba92c70b12389457d01915b
Fire-Fairy84 <esther.sarasua@gmail.com>
2024-10-02 20:49:11 +0200

    create endpoint for filter sessions by project

--- projects/urls.py
+++ projects/urls.py
@@ -1,18 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (ProjectViewSet, SessionViewSet, InterestedParticipantViewSet, get_suggested_developers,
-                    get_developer_public_data, get_developer_private_data, get_suggested_sessions_for_user)
+                    get_developer_public_data, get_developer_private_data, get_suggested_sessions_for_user,
+                    SessionsByProjectView)
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet)
 router.register(r'sessions', SessionViewSet)
 router.register(r'interested-participants', InterestedParticipantViewSet)
 
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('project-sessions/<int:project_id>/', SessionsByProjectView.as_view(), name='sessions_by_project'),
     path('sessions/<int:session_id>/suggested-developers/', get_suggested_developers, name='suggested_developers'),
-    path('sessions/<int:session_id>/developers/<int:developer_id>/public/', get_developer_public_data, name='developer_public_data'),
-    path('sessions/<int:session_id>/developers/<int:developer_id>/private/', get_developer_private_data, name='developer_private_data'),
+    path('sessions/<int:session_id>/developers/<int:developer_id>/public/', get_developer_public_data,
+         name='developer_public_data'),
+    path('sessions/<int:session_id>/developers/<int:developer_id>/private/', get_developer_private_data,
+         name='developer_private_data'),
     path('users/suggested-sessions/', get_suggested_sessions_for_user, name='suggested_sessions'),
+    path('projects/<int:project_id>/sessions/', SessionsByProjectView.as_view(), name='sessions_by_project'),
+    path('', include(router.urls)),
 ]
