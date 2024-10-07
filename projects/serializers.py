3ebe7ece99ddeed02b14a48d395a8ed1b2b190bf
Lynn Poh <pohwlynn@gmail.com>
2024-10-07 14:39:15 +0200

    changed order of SessionSerializer to before ProjectSerializer

--- projects/serializers.py
+++ projects/serializers.py
@@ -4,54 +4,6 @@ from users.models import CustomUser
 from .models import Project, Session, InterestedParticipant
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    owner_name = serializers.CharField(source='owner.username', read_only=True)
-    owner_id = serializers.PrimaryKeyRelatedField(source='owner', read_only=True)
-    owner_avatar_url = serializers.CharField(source='owner.photo', read_only=True)
-    image_url = serializers.CharField(source='image.url', read_only=True)  # Ya no es SerializerMethodField
-    stack = serializers.PrimaryKeyRelatedField(queryset=Stack.objects.all(), write_only=True)
-    stack_name = serializers.CharField(source='stack.name', read_only=True)
-    languages = serializers.PrimaryKeyRelatedField(many=True, queryset=ProgLanguage.objects.all(), write_only=True)
-    language_names = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name', source='languages')
-    level = serializers.PrimaryKeyRelatedField(queryset=Level.objects.all(), write_only=True)
-    level_name = serializers.CharField(source='level.name', read_only=True)
-
-    class Meta:
-        model = Project
-        fields = ['id', 'name', 'description', 'image', 'stack', 'stack_name', 'languages', 'language_names', 'level',
-                  'level_name', 'image_url', 'owner_id', 'owner_name', 'owner_avatar_url']
-
-    def create(self, validated_data):
-        languages_data = validated_data.pop('languages')
-        project = Project.objects.create(owner=self.context['request'].user, **validated_data)
-        project.languages.set(languages_data)
-        return project
-
-    def update(self, instance, validated_data):
-        languages_data = validated_data.pop('languages', None)
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        if languages_data is not None:
-            instance.languages.set(languages_data)
-        instance.save()
-        return instance
-
-    def validate_stack(self, value):
-        if not Stack.objects.filter(id=value.id).exists():
-            raise serializers.ValidationError("Selected stack does not exist.")
-        return value
-
-    def validate_level(self, value):
-        if not Level.objects.filter(id=value.id).exists():
-            raise serializers.ValidationError("Selected level does not exist.")
-        return value
-
-    def validate_languages(self, value):
-        if not value:
-            raise serializers.ValidationError("At least one language must be selected.")
-        return value
-
-
 class SessionSerializer(serializers.ModelSerializer):
     owner_id = serializers.PrimaryKeyRelatedField(source='host', read_only=True)
     owner_name = serializers.CharField(source='host.username', read_only=True)
@@ -87,6 +39,54 @@ class SessionSerializer(serializers.ModelSerializer):
             if language.id not in project_languages_ids:
                 raise serializers.ValidationError(f"Language {language.name} is not part of the project.")
 
+        return value
+
+class ProjectSerializer(serializers.ModelSerializer):
+    owner_name = serializers.CharField(source='owner.username', read_only=True)
+    owner_id = serializers.PrimaryKeyRelatedField(source='owner', read_only=True)
+    owner_avatar_url = serializers.CharField(source='owner.photo', read_only=True)
+    image_url = serializers.CharField(source='image.url', read_only=True)  # Ya no es SerializerMethodField
+    stack = serializers.PrimaryKeyRelatedField(queryset=Stack.objects.all(), write_only=True)
+    stack_name = serializers.CharField(source='stack.name', read_only=True)
+    languages = serializers.PrimaryKeyRelatedField(many=True, queryset=ProgLanguage.objects.all(), write_only=True)
+    language_names = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name', source='languages')
+    level = serializers.PrimaryKeyRelatedField(queryset=Level.objects.all(), write_only=True)
+    level_name = serializers.CharField(source='level.name', read_only=True)
+    sessions = SessionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'description', 'image', 'stack', 'stack_name', 'languages', 'language_names', 'level',
+                  'level_name', 'image_url', 'owner_id', 'owner_name', 'owner_avatar_url', 'sessions']
+
+    def create(self, validated_data):
+        languages_data = validated_data.pop('languages')
+        project = Project.objects.create(owner=self.context['request'].user, **validated_data)
+        project.languages.set(languages_data)
+        return project
+
+    def update(self, instance, validated_data):
+        languages_data = validated_data.pop('languages', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if languages_data is not None:
+            instance.languages.set(languages_data)
+        instance.save()
+        return instance
+
+    def validate_stack(self, value):
+        if not Stack.objects.filter(id=value.id).exists():
+            raise serializers.ValidationError("Selected stack does not exist.")
+        return value
+
+    def validate_level(self, value):
+        if not Level.objects.filter(id=value.id).exists():
+            raise serializers.ValidationError("Selected level does not exist.")
+        return value
+
+    def validate_languages(self, value):
+        if not value:
+            raise serializers.ValidationError("At least one language must be selected.")
         return value
 
 
